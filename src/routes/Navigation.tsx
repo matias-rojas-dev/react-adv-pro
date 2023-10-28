ccdeca10951ef363fa90dc01788036fdaba1020d
Matias Rojas <mrojasr@utem.cl>
2023-10-27 21:21:49 -0300

    I implemented an index.tsx file to handle page paths.

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, NavLink, Routes, Route, Navigate } from 'react-router-dom'
 
 import Logo from '../logo.svg'
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages'
+
 
 export const Navigation = () => {
   return <BrowserRouter>
@@ -9,21 +11,21 @@ export const Navigation = () => {
         <img src={Logo} alt='Logo de carga ' />
         <ul>
           <li>
-            <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink>
+            <NavLink to="/lazy1" className={({ isActive }) => isActive ? 'nav-active' : ''}>Lazy 1</NavLink>
           </li>
           <li>
-            <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
+            <NavLink to="/lazy2" className={({ isActive }) => isActive ? 'nav-active' : ''}>Lazy 2</NavLink>
           </li>
           <li>
-            <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
+            <NavLink to="/lazy3" className={({ isActive }) => isActive ? 'nav-active' : ''}>Lazy 3</NavLink>
           </li>
         </ul>
       </nav>
 
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/users" element={<h1>Users Page</h1>} />
-        <Route path="/about" element={<h1>About Page</h1>} />
+        <Route path="/lazy1" element={<LazyPage1 />} />
+        <Route path="/lazy2" element={<LazyPage2 />} />
+        <Route path="/lazy3" element={<LazyPage3 />} />
         <Route path="/*" element={<Navigate to="/home" replace />} />
       </Routes>
 
